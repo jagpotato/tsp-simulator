fe3a7651a16d459df2cd3429195e4d6806b57d0d
daiki <dik.piyo.piyo@gmail.com>
2018-05-24 01:08:40 +0900

    add assets/js

---
+++ src/assets/Tsp.js
@@ -0,0 +1,8 @@
+export default class {
+  constructor () {
+    this.message = 'Hello'
+  }
+  hello () {
+    return this.message
+  }
+}
---
+++ src/components/Main.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>{{this.value}}</div>
+</template>
+
+<script>
+import Tsp from '@/assets/Tsp'
+
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  computed: {
+    value () {
+      const t = new Tsp()
+      return t.hello()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
